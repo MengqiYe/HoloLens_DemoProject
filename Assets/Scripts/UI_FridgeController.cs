123ee53d6e9dfc726e9a50424ab376351cf0047a
seaswander <sevenseaswander@sina.cn>
2016-09-25 17:53:19 +0800

    添加找来的冰箱模型，添加的动画和控制脚本。

---
+++ Assets/Scripts/UI_FridgeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_FridgeController : MonoBehaviour
+{
+    private Animator _animator;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TriggerOpen()
+    {
+        _animator.SetTrigger("TriggerOpen");
+    }
+
+    public void TiggerClose()
+    {
+        _animator.SetTrigger("TriggerClose");
+    }
+}
