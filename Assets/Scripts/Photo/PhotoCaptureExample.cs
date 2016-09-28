dd24b22f94b0d91281c806546f8965073535b512
seaswander <sevenseaswander@sina.cn>
2016-09-28 22:45:51 +0800

    Test PhotoCapture

---
+++ Assets/Scripts/Photo/PhotoCaptureExample.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.VR.WSA.WebCam;
+
+public class PhotoCaptureExample : MonoBehaviour
+{
+    public GameObject[] GameObjects;
+    public Material Mat;
+    PhotoCapture photoCaptureObject = null;
+    Texture2D targetTexture = null;
+
+    // Use this for initialization
+    public void TakePicture()
+    {
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        // Create a PhotoCapture object
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Activate the camera
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, false, delegate (PhotoCapture.PhotoCaptureResult result) {
+                // Take a picture
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            });
+        });
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        // Copy the raw image data into our target texture
+        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+
+        // Create a gameobject that we can apply our texture to
+        Renderer quadRenderer = GameObjects[0].GetComponent<Renderer>() as Renderer;
+        quadRenderer.material = new Material(Shader.Find("Unlit/Texture"));
+
+        GameObjects[0].transform.parent = this.transform;
+        quadRenderer.material.SetTexture("_MainTex", targetTexture);
+
+        // Create a gameobject that we can apply our texture to
+        Renderer quadRenderer2 = GameObjects[1].GetComponent<Renderer>() as Renderer;
+        quadRenderer2.material = Mat;
+
+        GameObjects[1].transform.parent = this.transform;
+        quadRenderer2.material.SetTexture("_MainTex", targetTexture);
+
+        // Deactivate our camera
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+    }
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        // Shutdown our photo capture resource
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+}
---
+++ Assets/Scripts/Photo/ShowText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+    // Use this for initialization
+    public void Show(string t)
+    {
+        text.text = t;
+    }
+}
